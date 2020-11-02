fe0bb90a878a4ee926451979e97d3af98adcbf05
Neil Danson <neil.danson@gmail.com>
2020-11-02 20:19:36 +0000

    Code re-org to support benching (not yet working :( )

--- src/ray.rs
+++
@@ -1,7 +0,0 @@
-use glam::Vec3A;
-
-#[derive(Copy, Clone)]
-pub struct Ray {
-    pub position: Vec3A,
-    pub direction: Vec3A,
-}
--- src/shape.rs
+++
@@ -1,7 +0,0 @@
-use crate::{Intersection, Ray};
-use glam::Vec3A;
-
-pub trait Shape {
-    fn normal(&self, position: Vec3A) -> Vec3A;
-    fn intersects(&self, ray: Ray) -> Option<Intersection>;
-}
